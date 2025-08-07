eaada1b8598858cce29934829190f853127f6a36
magis-techno <magis.techno@gmail.com>
2025-08-07 21:18:54 +0800

    update

--- scripts/miscs/gen_pdm_score.sh
+++ scripts/miscs/gen_pdm_score.sh
@@ -17,9 +17,9 @@ echo "✅ Metric cache found at $METRIC_CACHE_DIR"
 
 python scripts/miscs/gen_multi_trajs_pdm_score.py \
 agent=WoTE_agent \
-'agent.checkpoint_path="$NAVSIM_EXP_ROOT/WoTE/default/lightning_logs/version_0/checkpoints/epoch=29-step=9990.ckpt"' \
+agent.checkpoint_path="$NAVSIM_EXP_ROOT/WoTE/default/lightning_logs/version_0/checkpoints/epoch=29-step=9990.ckpt" \
 agent.config._target_=navsim.agents.WoTE.configs.default.WoTEConfig \
 experiment_name=eval/gen_data \
-metric_cache_path='$NAVSIM_EXP_ROOT/metric_cache' \
+metric_cache_path="$NAVSIM_EXP_ROOT/metric_cache" \
 split=trainval \
-scene_filter=navtrain \
+scene_filter=navtrain
